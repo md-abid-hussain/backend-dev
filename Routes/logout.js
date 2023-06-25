956f4d30b80625de70736ec10aa2955b02d970ce
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-25 17:25:13 +0530

    updated method for refresh and logout endpoint

--- Routes/logout.js
+++ Routes/logout.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router();
 const { handleLogout } = require('../controllers/logoutController')
 
-router.post('/', handleLogout);
+router.get('/', handleLogout);
 
 module.exports = router
--- Routes/refresh.js
+++ Routes/refresh.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { handleRefreshToken } = require('../controllers/refreshTokenController');
 
-router.post('/', handleRefreshToken);
+router.get('/', handleRefreshToken);
 
 module.exports = router;
